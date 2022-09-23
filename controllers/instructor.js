0856536a0efcfa7f3661aea87a52573a99d8bce0
arfat-beep <arfatrahman08@gmail.com>
2022-09-23 20:13:55 +0600

    current instructor dashboard created with protected route

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -60,3 +60,16 @@ export const getAccountStatus = async (req, res) => {
     console.log("Errro from getAccountStatus =>", err);
   }
 };
+
+export const currentInstructor = async (req, res) => {
+  try {
+    let user = await User.findById(req.auth._id).select("-password").exec();
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      res.json({ ok: true });
+    }
+  } catch (err) {
+    console.log("error from currentInstructor =>", err);
+  }
+};
--- routes/instructor.js
+++ routes/instructor.js
@@ -1,12 +1,17 @@
 import express from "express";
 const router = express.Router();
 // controllers
-import { makeInstructor, getAccountStatus } from "../controllers/instructor";
+import {
+  makeInstructor,
+  getAccountStatus,
+  currentInstructor,
+} from "../controllers/instructor";
 
 // middlewares
 import { requireSignin } from "../middlewares";
 
 router.post("/make-instructor", requireSignin, makeInstructor);
 router.post("/get-account-status", requireSignin, getAccountStatus);
+router.get("/current-instructor", requireSignin, currentInstructor);
 
 module.exports = router;
